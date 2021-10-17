922af2391e1a86e70a09375c1e0b8fa1aaff98dc
Stas <stas20stas@gmail.com>
2021-10-18 00:23:17 +0300

    Добавил утилиту date для работы с библиотекой moment

---
+++ src/utils/date.js
@@ -0,0 +1,17 @@
+import moment from "moment";
+
+export const formatDateYear = (date) => {
+  return moment(new Date(date)).format(`YYYY`);
+};
+
+export const formatDateFullDate = (date) => {
+  return moment(new Date(date)).format(`DD MMMM YYYY`);
+};
+
+export const formatDuration = (durationMinutes) => {
+  return moment.unix(durationMinutes * 60).utc().format(`H[h] mm[m]`);
+};
+
+export const formatDateHumanReadable = (date) => {
+  return moment(date, `YYYY-MM-DDTHH:mm:ss.SSS-3`).fromNow();
+};
